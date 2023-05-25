c62de1d48824a59894547dee9851fcbc8cc443b5
Muden <mudennn@yahoo.com>
2023-05-25 16:29:34 +0800

    update about us page

---
+++ src/data/ekosistemData.js
@@ -0,0 +1,34 @@
+const ekosistem = [
+    {
+      name: "Skola labs tech",
+      description: "Cabang pembangunan MySekolahKripto. Memupuk generasi baharu bakat dalam pengaturcaraan dan kes penggunaan sebenar dalam Web3, Blockchain dan DeFi.",
+      
+    },
+    {
+      name: "Osmosis Malaysia",
+      description: "Berfungsi sebagai saluran komunikasi untuk pemegang & peniaga syiling Osmosis di Malaysia.",
+      
+    },
+    {
+      name: "Waves degen Malaysia",
+      description: "Tempat untuk meneroka Waves dan mengetahui lebih lanjut tentang DeFi dalam Ekosistem Waves untuk rakyat Malaysia.",
+      
+    },
+    {
+      name: "Tezos degen Malaysia",
+      description: "Jom ceburi Tezos bersama Tezos Degen Malaysia!",
+      
+    },
+    {
+      name: "Malaysia Interchain Association",
+      description: "Memberi tumpuan kepada pembinaan komuniti dalam Ekosistem Kosmos.",
+      
+    },
+    {
+      name: "W3nita	",
+      description: "Memperkasakan wanita dalam ruang Web3 dan Blockchain di Malaysia.",
+      
+    },
+  ];
+
+  export default ekosistem;
