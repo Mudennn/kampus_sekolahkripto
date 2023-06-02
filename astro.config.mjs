f8d81af59e4e0f76907b9d4a0e9321df29ee3a6b
Muden <mudennn@yahoo.com>
2023-06-02 23:28:53 +0800

    update vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), tailwind()],
-  
+  output: "server",
+  adapter: vercel()
 });
